f9bd212ca73fd81ff0fa41286b548a729b633045
Patrick Cole <hpcmobiledev@gmail.com>
2018-07-08 15:30:08 -0400

    Updates journal with Webpack and Vue code practice - Part 1
    * This commit represents the end of part 1 of the code practice.
    * node_modules have been excluded to save space in repo.

---
+++ 2018/07/08/code/src/Hello.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+  export default {
+    name: "Hello",
+    data() {
+      return {
+        msg: "Hello"
+      }
+    }
+  }
+</script>
